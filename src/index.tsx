54fc1dd66d49f044425fd721b96619c4f04f2758
Jaiwanth <jaiwanth2011@gmail.com>
2020-10-12 19:27:51 +0530

    Revert to commit a172411

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { hydrate, render } from "react-dom";
 
-const rootElement = document.getElementById("root");
-if (rootElement?.hasChildNodes())
-  hydrate(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    rootElement
-  );
-else
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    rootElement
-  );
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
