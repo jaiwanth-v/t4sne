c70afbb9c48257ad6c95a422251f0f0039fb7432
Jaiwanth <jaiwanth2011@gmail.com>
2020-10-12 19:15:27 +0530

    Added react-snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes())
+  hydrate(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
+else
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
 
 serviceWorker.unregister();
